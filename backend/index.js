fa5460ced21cb32fb1f1d49716b84e5770d81c16
Nam321 <evgenisnam@gmail.com>
2024-06-13 22:34:42 +0300

    3rd commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
